4fac7eb56c43664b91836bd8f22e89475f235200
7kaji <hirotonbuu@gmail.com>
2019-06-24 23:37:48 +0900

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :valid_request?
+
+  def valid_request?
+    token = request.headers['Authorization'].try(:split, 'Bearer ')
+    head :unauthorized and return if token.blank?
+
+    user = ::CognitoJwtVerifier.new(token[1])
+    if user.validate_and_decrypt == false
+      head :unauthorized and return
+    end
+  end
 end
