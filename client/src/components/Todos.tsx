b3f6b4f174d42897c84dba1d434ac53cb3367b34
7kaji <hirotonbuu@gmail.com>
2019-07-01 23:08:38 +0900

    lint and prettier

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,31 +1,59 @@
+/* eslint-disable no-console */
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import parse from 'parse-link-header'
 import queryString from 'query-string'
 import Button from '@material-ui/core/Button'
 
-interface ITodo {
-  id: number
-  title: string
-  done: boolean
-  // created_at: string
-  // updated_at: string
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
 }
 
-interface IPage {
-  page: string
-  rel: string
-  url: string
+interface Page {
+  page: string;
+  rel: string;
+  url: string;
 }
 
-function Todos(): JSX.Element {
+const Todos: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('')
-  const [todos, setTodos] = useState<ITodo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([])
   const [currentPage, setCurrentPage] = useState<string>('1')
-  const [prevPage, setPrevPage] = useState<IPage>({page: '0', rel: '', url: ''})
-  const [nextPage, setNextPage] = useState<IPage>({page: '-1', rel: '', url: ''})
+  const [prevPage, setPrevPage] = useState<Page>({page: '0', rel: '', url: ''})
+  const [nextPage, setNextPage] = useState<Page>({page: '-1', rel: '', url: ''})
   const [totalPage, setTotalPage] = useState<number>(0)
   // const [totalCount, setTotalCount] = useState<number>(0)
+
+  const getTodos = async(page: string) => {
+    const stringified = queryString.stringify({page})
+    const pageParamsString = (page === '1') ? '' : `?${stringified}`
+    await axios.get(`/api/v1/todos${pageParamsString}`)
+      .then(response => {
+        const parsedLinkHeaders = parse(response.headers.link) || {}
+
+        // pagination
+        if (parsedLinkHeaders.prev) {
+          const prev = parsedLinkHeaders.prev
+          setPrevPage({page: prev.page, rel: prev.rel, url: prev.url})
+        } else {
+          setPrevPage({page: '0', rel: '', url: ''})
+        }
+        if (parsedLinkHeaders.next) {
+          const next = parsedLinkHeaders.next
+          setNextPage({page: next.page, rel: next.rel, url: next.url})
+        } else {
+          setNextPage({page: '-1', rel: '', url: ''})
+        }
+        setCurrentPage(response.headers['x-page'])
+        // setTotalCount(response.headers['x-total'])
+        setTotalPage(Math.ceil(Number(response.headers['x-total']) / response.headers['x-per-page']))
+
+        setTodos(response.data)
+      })
+      .catch(error => console.error(error))
+  }
 
   useEffect(
     () => {
@@ -34,43 +62,15 @@ function Todos(): JSX.Element {
     }, []
   )
 
-  const getTodos = async(page: string) => {
-    const stringified = queryString.stringify({page})
-    const pageParamsString = (page === '1') ? '' : `?${stringified}`
-    await axios.get(`/api/v1/todos${pageParamsString}`)
-    .then(response => {
-      const parsedLinkHeaders = parse(response.headers.link) || {}
-
-      // pagination
-      if (parsedLinkHeaders.prev) {
-        const prev = parsedLinkHeaders.prev
-        setPrevPage({page: prev.page, rel: prev.rel, url: prev.url})
-      } else {
-        setPrevPage({page: '0', rel: '', url: ''})
-      }
-      if (parsedLinkHeaders.next) {
-        const next = parsedLinkHeaders.next
-        setNextPage({page: next.page, rel: next.rel, url: next.url})
-      } else {
-        setNextPage({page: '-1', rel: '', url: ''})
-      }
-      setCurrentPage(response.headers['x-page'])
-      // setTotalCount(response.headers['x-total'])
-      setTotalPage(Math.ceil(Number(response.headers['x-total']) / response.headers['x-per-page']))
-
-      setTodos(response.data)
-    })
-    .catch(error => console.log(error))
-  }
 
   const createTodo = (e: React.KeyboardEvent<HTMLDivElement>): void => {
     if (e.key === 'Enter' && !(inputValue === '')) {
       axios.post('/api/v1/todos', {todo: {title: inputValue}})
-      .then(response => {
-        setTodos([response.data, ...todos])
-        setInputValue('')
-      })
-      .catch(error => console.log(error))
+        .then(response => {
+          setTodos([response.data, ...todos])
+          setInputValue('')
+        })
+        .catch(error => console.log(error))
     }
   }
 
@@ -86,20 +86,20 @@ function Todos(): JSX.Element {
 
   const updateTodo = (e: React.ChangeEvent<HTMLInputElement>, id: number): void => {
     axios.put(`/api/v1/todos/${id}`, {todo: {done: e.target.checked}})
-    .then(response => {
-      const todoIndex = todos.findIndex(x => x.id === response.data.id)
-      setTodos([...todos.slice(0, todoIndex), response.data, ...todos.slice(todoIndex + 1)])
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        const todoIndex = todos.findIndex(x => x.id === response.data.id)
+        setTodos([...todos.slice(0, todoIndex), response.data, ...todos.slice(todoIndex + 1)])
+      })
+      .catch(error => console.log(error))
   }
 
   const deleteTodo = (id: number): void => {
     axios.delete(`/api/v1/todos/${id}`)
-    .then(response => {
-      const todoIndex = todos.findIndex(x => x.id === id)
-      setTodos([...todos.slice(0, todoIndex), ...todos.slice(todoIndex + 1)])
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        const todoIndex = todos.findIndex(x => x.id === id)
+        setTodos([...todos.slice(0, todoIndex), ...todos.slice(todoIndex + 1)])
+      })
+      .catch(error => console.log(error))
   }
 
   return (
